9c039b7c7e40ef6641bb2a0d8f82c1ea0d5f46e5
Anil <yusufanilturgut22@gmail.com>
2022-10-27 10:26:32 +0300

    commit

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -4,12 +4,27 @@ public class Array {
 	public static void main(String[] args) 
 	{
 		int[] array = {32, 27, 64, 18, 95, 14, 90, 70,60, 37};
-
+		int temp;
+		int len;
+		len = array.length - 1;
 		System.out.printf("%s%8s%n", "Index", "Value");
 		for(int counter = 0; counter < array.length;counter++)
 		{
 			System.out.printf("%5d%8d%n",counter,array[counter]);
 		}
+		System.out.printf("%s%8s Reverse%n", "Index", "Value");
+		for(int counter = 0; counter < len;counter++)
+		{
+			temp = array[counter];
+			array[counter] = array[len];
+			array[len] = temp;
+			len--;
+		}	
+		for(int counter = 0; counter < array.length;counter++)
+		{
+			System.out.printf("%5d%8d%n",counter,array[counter]);
+		}
+
 	}
 
 }
