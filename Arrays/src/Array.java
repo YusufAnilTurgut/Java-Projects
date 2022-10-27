1ef0c5ba944195fa254825efa0dc3a83d628ff64
Anil <yusufanilturgut22@gmail.com>
2022-10-27 10:15:42 +0300

    Commit 04

--- Arrays/src/Array.java
+++ Arrays/src/Array.java
@@ -4,7 +4,7 @@ public class Array {
 	public static void main(String[] args) 
 	{
 		int[] array = {32, 27, 64, 18, 95, 14, 90, 70,60, 37};
-		System.out.printf("hello");
+
 		System.out.printf("%s%8s%n", "Index", "Value");
 		for(int counter = 0; counter < array.length;counter++)
 		{
